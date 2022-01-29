11615f927e5771d2312fff818ffebd26e9e83671
Tiziana Mancini <tizianabmancini@gmail.com>
2022-01-28 19:53:47 -0500

    Updated JavaScript to format code into React

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.js";
+import React from "react";
+import Search from "./Weather";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Weather Search Engine </h1>
+      <Weather />
+    </div>
+  );
+}
